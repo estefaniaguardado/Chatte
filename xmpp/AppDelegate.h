057235bd9ba77a2b07dd83d3efec0c6b280e74ba
Estefania Chavez Guardado <fachinacg@gmail.com>
2016-08-30 20:18:23 +0200

    Configure Paths

--- xmpp/AppDelegate.h
+++ xmpp/AppDelegate.h
@@ -7,11 +7,23 @@
 //
 
 #import <UIKit/UIKit.h>
+#import "XMPPFramework.h"
 
-@interface AppDelegate : UIResponder <UIApplicationDelegate>
+@protocol ChatDelegate
+
+- (void) buddyWentOnline: (NSString *) name;
+- (void) buddyWentOffline: (NSString *) name;
+- (void) didDisconnect;
+
+@end
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate, XMPPRosterDelegate, XMPPStreamDelegate>
 
 @property (strong, nonatomic) UIWindow *window;
-
+@property (weak) id<ChatDelegate> delegate; //var delegate:ChatDelegate! = nil
+@property (weak) XMPPStream * xmppStrem;
+@property (weak) XMPPRosterCoreDataStorage * xmppRosterStorage;
+@property (weak) XMPPRoster * xmppRoster;
 
 @end
 
