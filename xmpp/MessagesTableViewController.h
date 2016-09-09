b0ec1e796e6bfec806616eb36e571bd9d285ad4a
Estefania Chavez Guardado <fachinacg@gmail.com>
2016-09-09 15:08:28 +0200

    Add files for view of messages

---
+++ xmpp/MessagesTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  MessagesTableViewController.h
+//  xmpp
+//
+//  Created by Estefania Chavez Guardado on 9/9/16.
+//  Copyright © 2016 Estefania Chavez Guardado. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MessagesTableViewController : UITableViewController
+
+@end
